b3e49d3d09a6a361d75bc30085abed4f964b8bc5
hj8007 <rena_@kakao.com>
2021-10-07 23:38:16 +0900

    첫번째 과제(추가 수정)

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ let todoItems: Todo[];
 
 // api
 function fetchTodoItems(): Todo[] {
-  const todos = [
+  const todos: Todo[] = [
     { id: 1, title: '안녕', done: false },
     { id: 2, title: '타입', done: false },
     { id: 3, title: '스크립트', done: false },
